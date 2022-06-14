4074276addc8c92d3b2744921987f2527dd686a0
nbogdankov@gmail.com <nbogdankov@gmail.com>
2022-06-14 10:56:36 +0300

    added bundler for building and minifying code

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+
+const TerserPlugin = require("terser-webpack-plugin");
+
+module.exports = {
+    mode: "development",
+    devtool: "inline-source-map",
+    entry: "./script.ts",
+    output: {
+        path: __dirname,
+        filename: "bundle.js"
+    },
+    resolve: {
+      extensions: [".ts", ".tsx", ".js"]
+    },
+    module: {
+        rules: [
+            {
+              test: /\.tsx?$/,
+              use: [
+                {
+                  loader: 'ts-loader',
+                  options: {
+                    transpileOnly: true
+                  }
+                }
+              ]
+            }
+        ]
+    },
+    optimization: {
+        minimize: true,
+        minimizer: [new TerserPlugin()],
+      },
+  };
