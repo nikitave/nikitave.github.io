5d873d2f9d2871d30758c169e7eda81b1a196ce4
nbogdankov@gmail.com <nbogdankov@gmail.com>
2022-06-16 09:42:44 +0300

    little changes

--- webpack.config.js
+++ webpack.config.js
@@ -2,33 +2,33 @@
 const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = {
-    mode: "development",
-    devtool: "inline-source-map",
-    entry: "./script.ts",
-    output: {
-        path: __dirname,
-        filename: "bundle.js"
-    },
-    resolve: {
-      extensions: [".ts", ".tsx", ".js"]
-    },
-    module: {
-        rules: [
-            {
-              test: /\.tsx?$/,
-              use: [
-                {
-                  loader: 'ts-loader',
-                  options: {
-                    transpileOnly: true
-                  }
-                }
-              ]
+  mode: "development",
+  devtool: "inline-source-map",
+  entry: "./script.ts",
+  output: {
+    path: __dirname,
+    filename: "bundle.js"
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: [
+          {
+            loader: 'ts-loader',
+            options: {
+              transpileOnly: true
             }
+          }
         ]
-    },
-    optimization: {
-        minimize: true,
-        minimizer: [new TerserPlugin()],
-      },
-  };
+      }
+    ]
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserPlugin()],
+  },
+};
